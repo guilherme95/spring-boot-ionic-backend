6ff415e9f29a944efab9b8edd3f7931292848abb
guilherme95 <guilhermemello62@gmail.com>
2021-11-20 19:33:36 -0300

    custom validation: not repeteable email on insertion for Cliente model

--- src/main/java/com/guilherme/cursomc/services/ClienteService.java
+++ src/main/java/com/guilherme/cursomc/services/ClienteService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.guilherme.cursomc.domain.Cidade;
 import com.guilherme.cursomc.domain.Cliente;
@@ -37,6 +38,7 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = clienteRepository.save(obj);
--- src/main/java/com/guilherme/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/guilherme/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.guilherme.cursomc.domain.Cliente;
 import com.guilherme.cursomc.domain.enums.TipoCliente;
 import com.guilherme.cursomc.dto.ClienteNewDTO;
+import com.guilherme.cursomc.repositories.ClienteRepository;
 import com.guilherme.cursomc.resources.exception.FieldMessage;
 import com.guilherme.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente aux= clienteRepository.findByEmail(objDTO.getEmail());
+		if(aux!=null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
