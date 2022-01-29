765f75e35a277bb2860a6c14691c137541e79638
guilherme95 <guilhermemello62@gmail.com>
2022-01-29 16:51:27 -0300

    refresh token and adjustments

--- src/main/java/com/guilherme/cursomc/services/UserService.java
+++ src/main/java/com/guilherme/cursomc/services/UserService.java
@@ -7,7 +7,7 @@ public class UserService {
 
     public static UserSS authenticated() {
         try {
-            return (UserSS) SecurityContextHolder.getContext().getAuthentication();
+            return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         }catch(Exception ex){
             return null;
         }
